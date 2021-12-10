57632c85a37baae769003611abb0f113a68cf793
Luiza100000 <92116715+Luiza100000@users.noreply.github.com>
2021-12-10 08:08:21 +0300

    Update Main.java
    Сделала статический метод, который возвращает массив Cycles[]. (декомпозировала код) После в Main создала переменную Cycle и  массив cycles и вызвала переменную generateCycles(). Ведь два метода статичных, поэтому могу вызвать метод generateCycles() по наименованию. На 14, 18, 22 строчке лучше работать с переменной общего вида, которую закладываю в массив.

--- Main.java
+++ Main.java
@@ -1,46 +1,51 @@
-import java.util.*;
-
 public class Main {
+    public static Cycle[] generateCycles(){
+        int n = 10;
+        Cycle[] cycles = new Cycle[n];
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        Cycle[] cycle = new Cycle[n];
-        for(int i = 0; i < n; i++) {
-            int r = (int) (Math.random() * 3);
-            int d = (int) (Math.random() * 10);
-            switch (r) {
+        for (int i = 0; i < cycles.length; i++) {
+            int randomType = (int) (Math.random() * 3);
+            int randomDiameter = (int) (Math.random() * 10);
+
+            switch (randomType) {
                 case 0:
-                    Unicycle uni = new Unicycle(d);
-                    cycle[i] = uni;
+                    Cycle uni = new Unicycle(randomDiameter);
+                    cycles[i] = uni;
                     break;
                 case 1:
-                    Bicycle bic = new Bicycle(d);
-                    cycle[i] = bic;
+                    Cycle bic = new Bicycle(randomDiameter);
+                    cycles[i] = bic;
                     break;
                 case 2:
-                    Tricycle tri = new Tricycle(d);
-                    cycle[i] = tri;
+                    Cycle tri = new Tricycle(randomDiameter);
+                    cycles[i] = tri;
                     break;
             }
         }
-        for (int i=0; i<3; i++){
-            for (int j=1; j<3; j++){
-                if (cycle[j].getSumDiameter()<cycle[j-1].getSumDiameter()){
-                    Cycle sort = cycle[j];
-                    cycle[j] = cycle[j - 1];
-                    cycle[j - 1] = sort;
+        return cycles;
+    }
+
+    public static void main(String[] args) {
+        Cycle[] cycles = generateCycles();
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                if (cycles[j].getSumDiameter() < cycles[j - 1].getSumDiameter()) {
+                    Cycle sort = cycles[j];
+                    cycles[j] = cycles[j - 1];
+                    cycles[j - 1] = sort;
                 }
             }
         }
-        for(int i = 0;i<n;i++){
-            System.out.println(cycle[i].getSumDiameter());
+        for (int i = 0; i < 10; i++) {
+            System.out.println(cycles[i].getSumDiameter());
         }
-        for (Cycle c : cycle) {
-            if (c instanceof Unicycle)
+        for (Cycle c : cycles) {
+            if (c instanceof Unicycle) {
                 c.repair();
+            }
         }
-        Repair.repair_all(cycle[1]);
+        Repair.repair_all(cycles[1]);
     }
 
 }
